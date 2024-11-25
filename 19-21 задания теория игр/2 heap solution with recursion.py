c5274f91e46ac5039ed0a1106784820ab353160c
Mark Kaminskiy <107908712+200710@users.noreply.github.com>
2024-11-25 23:57:14 +0500

    Update 2 heap solution with recursion.py

--- 19-21 задания теория игр/2 heap solution with recursion.py
+++ 19-21 задания теория игр/2 heap solution with recursion.py
@@ -1,1 +1,16 @@
 from functools import *
+def m(s):
+  a, b = s
+  return (a*3, b), (a, b*3), (a+1, b), (a, b+1)
+@lru_cache(None)
+def g(s):
+  a, b = s
+  if a+b>45: return 'win'
+  if any(g(x)=='win' for x in m(s)): return 'p1'
+  if all(g(x)=='p1' for x in m(s)): return 'v1'
+  if any(g(x)=='v1' for x in m(s)): return 'p2'
+  if all(g(x)=='p1' or g(x)=='p2' for x in m(s)): return 'v2'
+a=4
+for b in range(44, 1, -1):
+  s=(a,b)
+  if g(s)=='v1': print(s, g(s))
